cdef5f7c053850124f5521090c5f5bf3188151ea
RuiKaga11 <104427410+RuiKaga11@users.noreply.github.com>
2022-07-18 23:52:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-'aaaaaaaa'
+# print('aaaaaaaa')
+from flask import Flask, render_template # 追加
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html') # 変更
+
+if __name__ == "__main__":
+    app.run(debug=True)
